1da58a7bc1d5ba3ff174ffedf11db92665c0b7d3
ThanhSaimon <thanhsaimon8@gmail.com>
2026-05-12 13:27:04 +0700

    Create 0005_ Integer_to_Roman.cpp

---
+++ String/0005_ Integer_to_Roman.cpp
@@ -0,0 +1,18 @@
+//runtime : 11ms
+// memory  11.95mb
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        string res = "";
+        for (int i = 0; i < values.size(); i++) {
+            while (num >= values[i]) {
+                res += symbols[i];
+                num -= values[i];
+            }
+        }
+        return res;
+    }
+};
