db1860fb9f8e450bb3f6027e746b8dd499271995
ThanhSaimon <thanhsaimon8@gmail.com>
2026-05-13 16:32:17 +0700

    Create 0001_Minimum_Moves_to_Make_Array_Complementary.cpp

---
+++ Prefix_Sum/0001_Minimum_Moves_to_Make_Array_Complementary.cpp
@@ -0,0 +1,33 @@
+// 13/5//2026
+// runtime : 0ms
+// memory : 93.42mb
+class Solution {
+public:
+    int minMoves(vector<int>& nums, int limit) {
+        int n = nums.size();
+        vector<int> diff(2 * limit + 2, 0);
+
+        for (int i = 0; i < n / 2; i++) {
+            int a = nums[i];
+            int b = nums[n - 1 - i];
+            int low = min(a, b) + 1;
+            int high = max(a, b) + limit;
+            int sum = a + b;
+
+            diff[2] += 2;
+            diff[low] -= 1;
+            diff[high + 1] += 1;
+            diff[sum] -= 1;
+            diff[sum + 1] += 1;
+        }
+
+        int res = INT_MAX;
+        int cur = 0;
+
+        for (int s = 2; s <= 2 * limit; s++) {
+            cur += diff[s];
+            res = min(res, cur);
+        }
+        return res;
+    }
+};
