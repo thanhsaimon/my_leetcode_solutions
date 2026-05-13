303242b37c3198e376a4214ea3d5d1543af8767a
ThanhSaimon <thanhsaimon8@gmail.com>
2026-05-13 14:38:36 +0700

    Create 0008_Valid_Parenthese.cpp

---
+++ String/0008_Valid_Parenthese.cpp
@@ -0,0 +1,24 @@
+// runtime : 0ms
+// memory : 8.8mb
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s){
+            if (c == '(' || c == '{' || c == '['){
+                st.push(c);
+            }
+            else {
+                if(st.empty()) return false;
+                char top = st.top();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                    }
+                st.pop();
+            }
+        }
+        return st.empty(); // valid after deleting everything
+    }
+};
