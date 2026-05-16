87e81805c34753453b2e52b28acfb0076f1fa5f4
ThanhSaimon <thanhsaimon8@gmail.com>
2026-05-16 15:25:42 +0700

    Create 0002_Find_Minimum_in_Rotated_Sorted_Array_II.cpp

---
+++ Binary_Search/0002_Find_Minimum_in_Rotated_Sorted_Array_II.cpp
@@ -0,0 +1,23 @@
+// 16/5/2026
+// runtime : 0ms
+// memory : 16.33mb
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left  = 0;
+        int right = nums.size() -1;
+        while (left < right){
+            int mid = (left + right) /2;
+            if (nums[mid] > nums[right]){
+                left = mid + 1;
+            }
+            else if (nums[mid] < nums[right]){
+                right = mid;
+            }
+            else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+};
