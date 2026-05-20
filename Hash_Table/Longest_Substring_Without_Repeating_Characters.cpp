4ac2f2de7b924ac6b678a860a2962d75537ff4c9
ThanhSaimon <thanhsaimon8@gmail.com>
2026-05-20 07:57:31 +0700

    Create Longest_Substring_Without_Repeating_Characters.cpp

---
+++ Hash_Table/Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,18 @@
+// runtime : 7ms
+// memory : 11.96mb
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char , int> cnt;
+        int l = 0;
+        int res = 0;
+        for (int r = 0 ; r < s.size() ; r++){
+            if (cnt.count(s[r])){
+                l = max(l,cnt[s[r]]+1);
+            }
+            cnt[s[r]] = r;
+            res = max(res, r - l + 1);
+        }
+    return res;
+    }
+};
