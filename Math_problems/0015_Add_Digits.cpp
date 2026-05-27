e6166b277d40fa5f1c8be80e6e456269c7e1b6fb
ThanhSaimon <thanhsaimon8@gmail.com>
2026-05-27 15:05:58 +0700

    Create 0015_Add_Digits.cpp

---
+++ Math_problems/0015_Add_Digits.cpp
@@ -0,0 +1,10 @@
+// 27/5/2026
+// without loop / recursion
+// -> Digital Root
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num == 0) return 0;
+        return 1 + ((num - 1) % 9); 
+    }
+};
